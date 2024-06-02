a62cdbec42f1e61b01160fcdb768bc141f396889
FighterSoul <hedi.abdessalem@gmail.com>
2024-06-02 15:44:40 +0100

    added driver.dto

--- src/dto/driver.dto.ts
+++ src/dto/driver.dto.ts
@@ -1,3 +1,23 @@
-export class DriverDto{
-    
+import { IsString, IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
+
+export class DriverDto {
+  @IsString()
+  @IsOptional()
+  readonly id?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly licenseNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly phone: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
 }
