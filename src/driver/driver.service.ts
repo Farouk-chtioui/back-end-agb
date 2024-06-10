aba097ce03c7bf6c9a01e8f88e0622d486b76ed2
farouk-chtioui <chtiouiaa25@gmail.com>
2024-06-10 11:32:45 +0100

    changed to dto instead of module

--- src/driver/driver.service.ts
+++ src/driver/driver.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Driver } from "src/schema/driver.schema";
-
+import { DriverDto } from "src/dto/driver.dto";
 @Injectable()
 export class DriverService{
     constructor(@InjectModel(Driver.name) private driverModel: Model<Driver>){}
-    async createDriver(driver: Driver): Promise<Driver>{
-        const newDriver = new this.driverModel(driver);
+    async createDriver(driverDto:DriverDto): Promise<Driver>{
+        const newDriver = new this.driverModel(driverDto);
         return newDriver.save();
     }
     async findAll(page: number = 1, limit: number = 6): Promise<Driver[]> {
@@ -17,9 +17,15 @@ export class DriverService{
     async findOne(id: string): Promise<Driver>{
         return this.driverModel.findById(id).exec();
     }
-    async update(id: string, driver: Driver): Promise<Driver>{
+    async update(id: string, driver: Driver): Promise<Driver> {
+        const existingDriver = await this.driverModel.findOne({ email: driver.email }).exec();
+      
+        if (existingDriver && existingDriver.id !== id) {
+          throw new Error('A driver with this email already exists');
+        }
+      
         return this.driverModel.findByIdAndUpdate(id, driver, {new: true}).exec();
-    }
+      }
     async delete(id: string): Promise<Driver>{
         if(!id) throw new Error('Id is required');
         if(!this.driverModel.findById(id)) throw new Error('Driver not found');
