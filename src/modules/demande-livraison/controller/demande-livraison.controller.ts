03ca059bb89aac12d9d8d5350bbd098bfda32ac9
farouk-chtioui <chtiouiaa25@gmail.com>
2024-07-17 10:42:30 +0100

    refactor: Add PATCH and DELETE methods to DemandeLivraisonController

--- src/modules/demande-livraison/controller/demande-livraison.controller.ts
+++ src/modules/demande-livraison/controller/demande-livraison.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
 import { DemandeLivraisonService } from '../services/demande-livraison.service';
 import { CreateDemandeLivraisonDto } from '../dto/create-demande-livraison.dto';
 import { UpdateDemandeLivraison } from '../dto/create-demande-livraison.dto.js';
@@ -26,4 +26,12 @@ export class DemandeLivraisonController {
     updateStatus(@Param('id') id: string, @Body() updateDemandeLivraisonStatusDto: UpdateDemandeLivraisonStatusDto) {
         return this.demandeLivraisonService.updateStatus(id, updateDemandeLivraisonStatusDto);
     }*/
+   @Patch(':id')
+    modifyDemandeLivraison(@Param('id') id: string, @Body() updateDemandeLivraison: UpdateDemandeLivraison) {
+        return this.demandeLivraisonService.modifyDemandeLivraison(id, updateDemandeLivraison);
+    }
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.demandeLivraisonService.delete(id);
+    }
 }
--- src/modules/demande-livraison/services/demande-livraison.service.ts
+++ src/modules/demande-livraison/services/demande-livraison.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateDemandeLivraisonDto } from '../dto/create-demande-livraison.dto';
+import { CreateDemandeLivraisonDto, UpdateDemandeLivraison } from '../dto/create-demande-livraison.dto';
 // import { UpdateDemandeLivraisonStatusDto } from '../dto/update-demande-livraison-status.dto';
 import { DemandeLivraison } from '../schema/demande-livraison.schema';
 
@@ -24,7 +24,7 @@ export class DemandeLivraisonService {
             .limit(perPage)
             .populate('client', '-password')
             .populate({ path: 'products.productId', model: 'Product' })
-            .populate('market')
+            .populate('market','-password')
             .populate('driver')
             .exec();
     }
@@ -36,4 +36,10 @@ export class DemandeLivraisonService {
     // async updateStatus(id: string, updateDemandeLivraisonStatusDto: UpdateDemandeLivraisonStatusDto): Promise<DemandeLivraison> {
     //     return this.demandeLivraisonModel.findByIdAndUpdate(id, { status: updateDemandeLivraisonStatusDto.status }, { new: true }).exec();
     // }
+    async delete(id: string): Promise<DemandeLivraison> {
+        return this.demandeLivraisonModel.findByIdAndDelete(id).exec();
+    }
+    async modifyDemandeLivraison(id: string, updateDemandeLivraison: UpdateDemandeLivraison): Promise<DemandeLivraison> {
+        return this.demandeLivraisonModel.findByIdAndUpdate(id, updateDemandeLivraison, { new: true }).exec();
+    }
 }
