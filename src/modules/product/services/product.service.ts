165548b7c3873a4a95e58fed63a110431de93af6
SameOldSteven <chtiouiaa25@gmail.com>
2024-08-19 22:58:28 +0100

    refactor: Update findAll method in ProductService to include pagination

--- src/modules/product/services/product.service.ts
+++ src/modules/product/services/product.service.ts
@@ -4,36 +4,53 @@ import { Model } from "mongoose";
 import { ProdcutDto } from "../dto/product.dto";
 import { Product } from "../schema/product.schema";
 import { ProductServiceInterface } from "../interfaces/product.interface";
+
 @Injectable()
 export class ProductService implements ProductServiceInterface {
     constructor(@InjectModel(Product.name) private productsModle: Model<Product>) {}
-    async createProduct(product: Product): Promise<Product>{
+
+    async createProduct(product: Product): Promise<Product> {
         const newProduct = new this.productsModle(product);
         return newProduct.save();
     }
+
     async create(product: ProdcutDto): Promise<Product> {
         const newProduct = new this.productsModle(product);
         return newProduct.save();
     }
+
     async findAllNoPage(): Promise<Product[]> {
         return this.productsModle.find().exec();
     }
-    async findAll(page: number = 1, limit: number = 6): Promise<Product[]> {
+
+    async findAll(page: number = 1, limit: number = 6): Promise<{ products: Product[], total: number, totalPages: number }> {
         const skip = (page - 1) * limit;
-        return this.productsModle.find().skip(skip).limit(limit).exec();
-      }
-    async findOne(id: string): Promise<Product>{
+        const total = await this.productsModle.countDocuments();
+        const products = await this.productsModle.find().skip(skip).limit(limit).exec();
+        const totalPages = Math.ceil(total / limit);
+
+        return {
+            products,
+            total,
+            totalPages,
+        };
+    }
+  
+    async findOne(id: string): Promise<Product> {
         return this.productsModle.findById(id).exec();
     }
+
     async update(id: string, product: ProdcutDto): Promise<Product> {
-        return this.productsModle.findByIdAndUpdate(id, product, {new: true}).exec();
+        return this.productsModle.findByIdAndUpdate(id, product, { new: true }).exec();
     }
-    async delete(id:string):Promise<Product>{
-        if(!id) throw new Error('Id is required');
-        if(!this.productsModle.findById(id)) throw new Error('Product not found');
+
+    async delete(id: string): Promise<Product> {
+        if (!id) throw new Error('Id is required');
+        if (!await this.productsModle.findById(id)) throw new Error('Product not found');
         return this.productsModle.findByIdAndDelete(id);
     }
-    async searchProduct(searchTerm: string): Promise<Product[]>{    
+
+    async searchProduct(searchTerm: string): Promise<Product[]> {    
         const regex = new RegExp(searchTerm, 'i');
         return this.productsModle.find({
             $or: [
@@ -42,5 +59,4 @@ export class ProductService implements ProductServiceInterface {
             ]
         }).exec();
     }
-    
 }
